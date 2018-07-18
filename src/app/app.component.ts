a3dedd125b1c0a8c94f7c7f53fb4180a034766a0
ChenZiyuJessie <250714751@qq.com>
2018-07-19 04:57:21 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  imageSrcroket=require('./images/roket.png');
+  imageSrcrobot=require('./images/robot.png');
+  imageSrctool1=require('./images/tool1.png');
+  imageSrctool2 = require('./images/tool2.png');
+  imageSrctool3 = require('./images/tool3.png');
+  imageSrctool4 = require('./images/tool4.png');
+  imageSrcfoot = require('./images/foot.png');
+  imageScrline=require('./images/line.png')
+
+
 }
